e775dbc15ce3d8704429e5804224b5407910569a
Christina Jansen <Christina.Jansen@informatik.rwth-aachen.de>
2017-07-28 10:15:11 +0200

    Predefined grammars + their class defs for renaming.

---
+++ src/main/assets/dataStructures/RenameDLL.java
@@ -0,0 +1,10 @@
+// Please adapt the following mapping here
+@Rename DLListNode -> YourDLListTypeHere
+@Rename next -> YourNextFieldNameHere
+@Rename prev -> YourPrevFieldNameHere
+
+public class DLListNode{
+
+	public DLListNode next;
+	public DLListNode prev;
+}
---
+++ src/main/assets/dataStructures/RenameSLL.java
@@ -0,0 +1,9 @@
+// Please adapt the following mapping here
+@Rename ListNode -> YourListTypeHere
+@Rename next -> YourNextFieldNameHere
+
+public class ListNode{
+
+	private ListNode next;
+	
+}
