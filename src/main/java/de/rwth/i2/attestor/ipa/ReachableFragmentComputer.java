2a8a6af3a449dc5ff3793c2b414755ef8b54a1ce
hannah.arndt <hannah.arndt@rwth-aachen.de>
2017-09-01 14:08:41 +0200

    reachableFragmentComputation basically implemented
    some cases might still fail
    not all tests yet implemented

---
+++ src/main/java/de/rwth/i2/attestor/ipa/ReachableFragmentComputer.java
@@ -0,0 +1,139 @@
+package de.rwth.i2.attestor.ipa;
+
+import java.util.*;
+
+import de.rwth.i2.attestor.graph.*;
+import de.rwth.i2.attestor.graph.heap.*;
+import de.rwth.i2.attestor.graph.heap.internal.InternalHeapConfiguration;
+import de.rwth.i2.attestor.types.Type;
+import de.rwth.i2.attestor.util.Pair;
+import gnu.trove.list.array.TIntArrayList;
+
+public class ReachableFragmentComputer {
+
+	
+	private String displayName;
+	
+	Queue<Integer> queue;
+	Map<Integer, Integer> idMapping;
+	HeapConfiguration input;
+	
+
+	public ReachableFragmentComputer(String displayName) {
+		super();
+		this.displayName = displayName;
+	}
+
+	protected Pair<HeapConfiguration, HeapConfiguration> prepareInput( HeapConfiguration input ){
+		
+		HeapConfigurationBuilder reachableFragmentBuilder = new InternalHeapConfiguration().builder();
+		HeapConfigurationBuilder replaceBuilder = input.clone().builder();
+		TIntArrayList externals = new TIntArrayList();
+		
+		queue = new ArrayDeque<>();
+		idMapping = new HashMap<>();
+		this.input = input;
+		
+		findParameterNodes(input, reachableFragmentBuilder, replaceBuilder);
+		computeReachableFragment(reachableFragmentBuilder, replaceBuilder, externals);
+		addIpaNonterminal(replaceBuilder, externals);
+		
+		return new Pair<HeapConfiguration, HeapConfiguration>(reachableFragmentBuilder.build(), replaceBuilder.build() );
+	}
+
+	private void addIpaNonterminal(HeapConfigurationBuilder replaceBuilder, TIntArrayList externals) {
+		final int rank = externals.size();
+		final boolean[] isReductionTentacle = new boolean[rank];
+		Nonterminal nt = BasicNonterminal.getNonterminal(displayName + rank, rank, isReductionTentacle);
+		
+		NonterminalEdgeBuilder edgeBuilder = replaceBuilder.addNonterminalEdge(nt);
+		for( int i = 0; i < externals.size(); i++ ){
+			edgeBuilder.addTentacle(externals.get(i) );
+		}
+		edgeBuilder.build();
+	}
+
+
+	private void computeReachableFragment(HeapConfigurationBuilder reachableFragmentBuilder, HeapConfigurationBuilder replaceBuilder,
+			TIntArrayList externals) {
+		while( ! queue.isEmpty() ){
+			
+			int nodeId = queue.poll();
+			
+			handleNonterminalEdges(input, reachableFragmentBuilder, replaceBuilder, queue, idMapping, nodeId);		
+			handleSelectorEdges(input, reachableFragmentBuilder, replaceBuilder, queue, idMapping, nodeId);
+			
+			if( ! input.attachedVariablesOf(nodeId).isEmpty() ){
+				reachableFragmentBuilder.setExternal( idMapping.get(nodeId ) );
+				externals.add(nodeId);
+			}else{
+				replaceBuilder.removeIsolatedNode(nodeId);
+			}
+
+		}
+	}
+
+
+	private void findParameterNodes(HeapConfiguration input, HeapConfigurationBuilder reachableFragmentBuilder,
+			HeapConfigurationBuilder replaceBuilder) {
+		TIntArrayList variables = input.variableEdges();
+		for( int i = 0; i < variables.size(); i++ ){
+			final int variableEdge = variables.get(i);
+			String variableName = input.nameOf( variableEdge );
+			if( variableName.startsWith("@param") || variableName.equals("@this") ){
+				replaceBuilder.removeVariableEdge(variableEdge );
+				
+				int targetedNode = input.targetOf( variableEdge );
+				if( ! idMapping.containsKey(targetedNode ) ){
+
+					queue.add( targetedNode );
+					addNodeToReachableFragment( targetedNode, reachableFragmentBuilder, input, idMapping );
+				}
+				
+				reachableFragmentBuilder.addVariableEdge(variableName, idMapping.get(targetedNode));
+			}
+		}
+	}
+
+	private void addNodeToReachableFragment(int targetedNode, HeapConfigurationBuilder reachableFragmentBuilder,
+			HeapConfiguration input, Map<Integer, Integer> idMapping) {
+		Type type = input.nodeTypeOf( targetedNode );
+		TIntArrayList insertedNode = new TIntArrayList();
+		reachableFragmentBuilder.addNodes(type, 1, insertedNode);
+		idMapping.put(targetedNode, insertedNode.get(0) );
+	}
+
+	private void handleSelectorEdges(HeapConfiguration input, HeapConfigurationBuilder reachableFragmentBuilder,
+			HeapConfigurationBuilder replaceBuilder, Queue<Integer> queue, Map<Integer, Integer> idOfInsertedNode,
+			int nodeId) {
+		for( SelectorLabel sel : input.selectorLabelsOf(nodeId) ){
+			int successorNode = input.selectorTargetOf(nodeId, sel);
+			if( ! idOfInsertedNode.containsKey(successorNode) ){
+				queue.add(successorNode );
+
+				addNodeToReachableFragment(successorNode, reachableFragmentBuilder, input, idOfInsertedNode);
+			}
+			reachableFragmentBuilder.addSelector(idOfInsertedNode.get(nodeId), sel, idOfInsertedNode.get(successorNode));
+			replaceBuilder.removeSelector(nodeId, sel);
+		}
+	}
+
+	private void handleNonterminalEdges(HeapConfiguration input, HeapConfigurationBuilder reachableFragmentBuilder,
+			HeapConfigurationBuilder replaceBuilder, Queue<Integer> queue, Map<Integer, Integer> idOfInsertedNode,
+			int nodeId) {
+		TIntArrayList nonterminalEdges = input.attachedNonterminalEdgesOf(nodeId);
+		for( int i = 0; i < nonterminalEdges.size(); i++ ){
+			int nonterminalEdge = nonterminalEdges.get(i);
+			TIntArrayList attachedNodes = input.attachedNodesOf(nonterminalEdge);
+			for( int n = 0; n < attachedNodes.size(); n++ ){
+				int attachedNode = attachedNodes.get( n );
+				if( ! idOfInsertedNode.containsKey(attachedNode) ){
+					queue.add( attachedNode );
+					
+					addNodeToReachableFragment(attachedNode, reachableFragmentBuilder, input, idOfInsertedNode);
+				}
+			}
+			replaceBuilder.removeNonterminalEdge( nonterminalEdge );
+		}
+	}
+}
--- src/test/java/de/rwth/i2/attestor/ipa/AbstractMethodIPATest.java
+++ src/test/java/de/rwth/i2/attestor/ipa/AbstractMethodIPATest.java
@@ -16,16 +16,81 @@ import gnu.trove.list.array.TIntArrayList;
 public class AbstractMethodIPATest {
 
 	AbstractMethodIPA ipa = new AbstractMethodIPA( "testMethod", null );
+	Type type = Settings.getInstance().factory().getType("someType");
+	SelectorLabel nextLabel = BasicSelectorLabel.getSelectorLabel("next");
 
 
 	@Test
-	public void testPrepareInput_Simple() {
-		HeapConfiguration input = singleNodeHeap();
-		HeapConfiguration expectedFragment = singleNodeExternal();
+	public void testPrepareInput_Simple_param() {
+		HeapConfiguration input = singleNodeHeap("@parameter0:");
+		HeapConfiguration expectedFragment = singleNodeExternal("@parameter0:");
 		HeapConfiguration expectedReplace = singleNodeAttached();
 
 		performTest(input, expectedFragment, expectedReplace);
 	}
+
+	@Test
+	public void testPrepareInput_Simple_this(){
+		HeapConfiguration input = singleNodeHeap("@this");
+		HeapConfiguration expectedFragment = singleNodeExternal("@this");
+		HeapConfiguration expectedReplace = singleNodeAttached();
+
+		performTest(input, expectedFragment, expectedReplace);
+	}
+
+	@Test
+	public void testPrepareInput_reachableList(){
+		for( int size = 2; size < 4; size++ ){
+			HeapConfiguration input = reachableList( size );
+			HeapConfiguration expectedFragment = reachableList_HeadExternal( size );
+			HeapConfiguration expectedReplace = singleNodeAttached();
+
+			performTest( input, expectedFragment, expectedReplace );
+		}
+
+	}
+
+	@Test
+	public void testPrepareInput_reachableList_withVariable(){
+		int listSize = 4;
+		int variablePosition = 2;
+		String variableName = "n1";
+		HeapConfiguration input = reachableListWithVariable( listSize, variablePosition, variableName );
+		HeapConfiguration expectedFragment = reachableList_HeadExternal_VarExternal( listSize, variablePosition );
+		HeapConfiguration expectedReplace = twoNodesAttached( variableName );
+
+		performTest( input, expectedFragment, expectedReplace );
+	}
+
+	@Test
+	public void testPrepareInput_partlyReachableList(){
+		for( int reachableListSize = 1; reachableListSize < 4; reachableListSize++ ){
+			for( int unreachableListSize = 1; unreachableListSize < 4; unreachableListSize++ ){
+
+				HeapConfiguration input = partlyReachableList( reachableListSize, unreachableListSize );
+				HeapConfiguration expectedFragment = reachableList_HeadExternal( reachableListSize );
+				HeapConfiguration expectedReplace = attachedToUnreachableList( unreachableListSize );
+
+				performTest( input, expectedFragment, expectedReplace );
+			}
+		}
+	}
+	
+	@Test
+	public void testPrepareInput_unreachableVariable(){
+		int reachableListSize = 1;
+		int unreachableListSize = 1;
+		String variableName = "n1";
+		int variablePosition = 0;
+		HeapConfiguration input = partlyReachableList_withVariable( reachableListSize, unreachableListSize,
+																	variableName, variablePosition );
+		HeapConfiguration expectedFragment = reachableList_HeadExternal( reachableListSize );
+		HeapConfiguration expectedReplace = attachedToUnreachableList_withVariable( unreachableListSize,
+																	variableName, variablePosition);
+
+		performTest( input, expectedFragment, expectedReplace );
+	}
+
 
 	private void performTest(HeapConfiguration input, HeapConfiguration expectedFragment,
 			HeapConfiguration expectedReplace) {
@@ -34,45 +99,90 @@ public class AbstractMethodIPATest {
 		assertEquals("replaced Fragment", expectedReplace, result.second());
 	}
 
-	@Test
-	public void testPrepareInput_reachableList(){
-		for( int size = 2; size < 4; size++ ){
-		HeapConfiguration input = reachableList( size );
-		HeapConfiguration expectedFragment = reachableList_HeadExternal( size );
-		HeapConfiguration expectedReplace = singleNodeAttached();
 
-		performTest( input, expectedFragment, expectedReplace );
-		}
-
+	private HeapConfiguration partlyReachableList(int reachableListSize, int unreachableListSize) {
+		TIntArrayList nodes = new TIntArrayList();
+		HeapConfiguration config = reachableListHelper( reachableListSize , nodes);
+		return addUnreachableList( unreachableListSize, config, nodes );
+	}
+	
+	private HeapConfiguration partlyReachableList_withVariable(int reachableListSize, int unreachableListSize,
+			String variableName, int variablePosition) {
+		TIntArrayList nodes = new TIntArrayList();
+		HeapConfiguration config = reachableListHelper( reachableListSize , nodes);
+		return addUnreachableListWithVariable( unreachableListSize, config, nodes, variableName, variablePosition );
 	}
 
-
-
-
-	private HeapConfiguration reachableList_HeadExternal(int size) {
-		HeapConfiguration hc = new InternalHeapConfiguration();
-
-		Type type = Settings.getInstance().factory().getType("someType");
-		SelectorLabel nextLabel = BasicSelectorLabel.getSelectorLabel("next");
-
+	private HeapConfiguration attachedToUnreachableList(int unreachableListSize) {
 		TIntArrayList nodes = new TIntArrayList();
-		HeapConfigurationBuilder builder =  hc.builder().addNodes(type, size, nodes)
-				.setExternal( nodes.get(0) )
-				.addVariableEdge("@parameter0:", nodes.get(0) );
-		for( int i = 0; i < size - 1; i++ ){
-			builder.addSelector(nodes.get(i), nextLabel, nodes.get(i + 1) );
-		}
+		HeapConfiguration config = singleNodeAttachedHelper( nodes );
+		return addUnreachableList(unreachableListSize, config, nodes );
+	}
+	
+	private HeapConfiguration attachedToUnreachableList_withVariable(int unreachableListSize, String variableName, int variablePosition){
+		TIntArrayList nodes = new TIntArrayList();
+		HeapConfiguration config = singleNodeAttachedHelper( nodes );
+		return  addUnreachableListWithVariable( unreachableListSize, config, nodes, variableName, variablePosition );
+	}
 
+	private HeapConfiguration addUnreachableListWithVariable( int unreachableListSize, 
+												HeapConfiguration config, TIntArrayList oldNodes,
+												String variableName, int variablePosition ) {
+		
+		TIntArrayList additionalNodes = new TIntArrayList();
+		HeapConfigurationBuilder builder = 
+				addUnreachableListHelper(unreachableListSize, config, oldNodes, additionalNodes)
+				.builder();
+		builder.addVariableEdge(variableName, additionalNodes.get(variablePosition) );
 		return builder.build();
 	}
 
-	private HeapConfiguration reachableList( int size) {
+	
+	private HeapConfiguration addUnreachableList(int unreachableListSize, HeapConfiguration config, TIntArrayList oldNodes) {
+		TIntArrayList additionalNodes = new TIntArrayList();
+		return addUnreachableListHelper(unreachableListSize, config, oldNodes, additionalNodes);
+	}
+
+	private HeapConfiguration addUnreachableListHelper(int unreachableListSize, HeapConfiguration config,
+			TIntArrayList oldNodes, TIntArrayList additionalNodes) {
+		HeapConfigurationBuilder builder = config.builder();
+		builder.addNodes(type, unreachableListSize, additionalNodes);
+
+		for( int i = 0; i < unreachableListSize -1; i++ ){
+			builder.addSelector(additionalNodes.get(i), nextLabel, additionalNodes.get(i +1 ) );
+		}
+
+		builder.addSelector(additionalNodes.get(unreachableListSize - 1), nextLabel, oldNodes.get(0) );
+		return builder.build();
+	}
+
+	private HeapConfiguration reachableListWithVariable(int listSize, int variablePosition, String variableName) {
+		TIntArrayList nodes = new TIntArrayList();
+		HeapConfiguration config = reachableListHelper( listSize , nodes);
+		return config.builder().addVariableEdge(variableName, nodes.get(variablePosition)).build();
+	}
+
+	private HeapConfiguration reachableList_HeadExternal_VarExternal(int listSize, int variablePosition) {
+		TIntArrayList nodes = new TIntArrayList();
+		HeapConfiguration config = reachableListHelper( listSize , nodes);
+		return config.builder().setExternal(nodes.get(0)).setExternal(nodes.get(variablePosition)).build();
+	}
+
+	private HeapConfiguration reachableList( int size ) {
+		TIntArrayList nodes = new TIntArrayList();
+		return reachableListHelper(size, nodes);
+	}
+
+	private HeapConfiguration reachableList_HeadExternal(int size) {
+		TIntArrayList nodes = new TIntArrayList();
+		HeapConfiguration config = reachableListHelper(size, nodes);
+		return config.builder().setExternal(nodes.get(0) ).build();
+	}
+
+
+	private HeapConfiguration reachableListHelper( int size, TIntArrayList nodes) {
 		HeapConfiguration hc = new InternalHeapConfiguration();
 
-		Type type = Settings.getInstance().factory().getType("someType");
-		SelectorLabel nextLabel = BasicSelectorLabel.getSelectorLabel("next");
-
-		TIntArrayList nodes = new TIntArrayList();
 		HeapConfigurationBuilder builder =  hc.builder().addNodes(type, size, nodes)
 				.addVariableEdge("@parameter0:", nodes.get(0) );
 		for( int i = 0; i < size - 1; i++ ){
@@ -83,14 +193,18 @@ public class AbstractMethodIPATest {
 	}
 
 	private HeapConfiguration singleNodeAttached() {
+		TIntArrayList nodes = new TIntArrayList();
+		return singleNodeAttachedHelper(nodes);
+	}
+
+	private HeapConfiguration singleNodeAttachedHelper(TIntArrayList nodes) {
 		HeapConfiguration hc = new InternalHeapConfiguration();
 
-		Type type = Settings.getInstance().factory().getType("someType");
 		final int rank = 1;
 		final boolean[] isReductionTentacle = new boolean[]{false};
 		Nonterminal nt = BasicNonterminal.getNonterminal(ipa.toString() + rank, rank, isReductionTentacle);
 
-		TIntArrayList nodes = new TIntArrayList();
+
 		return hc.builder().addNodes(type, rank, nodes)
 				.addNonterminalEdge(nt)
 				.addTentacle(nodes.get(0))
@@ -99,7 +213,39 @@ public class AbstractMethodIPATest {
 	}
 
 
-	private HeapConfiguration singleNodeExternal() {
+
+	private HeapConfiguration twoNodesAttached(String variableName) {
+		HeapConfiguration hc = new InternalHeapConfiguration();
+
+		Type type = Settings.getInstance().factory().getType("someType");
+		final int rank = 2;
+		final boolean[] isReductionTentacle = new boolean[rank];
+		Nonterminal nt = BasicNonterminal.getNonterminal(ipa.toString() + rank, rank, isReductionTentacle);
+
+		TIntArrayList nodes = new TIntArrayList();
+		return hc.builder().addNodes(type, rank, nodes)
+				.addVariableEdge(variableName, nodes.get(1))
+				.addNonterminalEdge(nt)
+				.addTentacle(nodes.get(0))
+				.addTentacle(nodes.get(1))
+				.build()
+				.build();
+	}
+
+
+	private HeapConfiguration singleNodeHeap(String variableName) {
+		HeapConfiguration hc = new InternalHeapConfiguration();
+
+		Type type = Settings.getInstance().factory().getType("someType");
+
+		TIntArrayList nodes = new TIntArrayList();
+		return hc.builder().addNodes(type, 1, nodes)
+				.addVariableEdge(variableName, nodes.get(0) )
+				.build();
+	}
+
+
+	private HeapConfiguration singleNodeExternal(String string) {
 		HeapConfiguration hc = new InternalHeapConfiguration();
 
 		Type type = Settings.getInstance().factory().getType("someType");
@@ -107,21 +253,11 @@ public class AbstractMethodIPATest {
 		TIntArrayList nodes = new TIntArrayList();
 		return hc.builder().addNodes(type, 1, nodes)
 				.setExternal(nodes.get(0))
-				.addVariableEdge("@parameter0:", nodes.get(0) )
+				.addVariableEdge(string, nodes.get(0) )
 				.build();
 	}
 
 
-	private HeapConfiguration singleNodeHeap() {
-		HeapConfiguration hc = new InternalHeapConfiguration();
-
-		Type type = Settings.getInstance().factory().getType("someType");
-
-		TIntArrayList nodes = new TIntArrayList();
-		return hc.builder().addNodes(type, 1, nodes)
-				.addVariableEdge("@parameter0:", nodes.get(0) )
-				.build();
-	}
 
 
 }
