edc24c0d60a4b9d62c2973a7d1e46320d811d76f
hannah.arndt <hannah.arndt@rwth-aachen.de>
2017-09-28 14:35:20 +0200

    some conceptional work
    doesnt compile yet

--- src/main/java/de/rwth/i2/attestor/ipa/AbstractMethodIPA.java
+++ src/main/java/de/rwth/i2/attestor/ipa/AbstractMethodIPA.java
@@ -1,14 +1,20 @@
 package de.rwth.i2.attestor.ipa;
 
-import java.util.Set;
+import java.util.*;
 
 import de.rwth.i2.attestor.graph.heap.HeapConfiguration;
 import de.rwth.i2.attestor.semantics.jimpleSemantics.jimple.statements.invoke.AbstractMethod;
 import de.rwth.i2.attestor.stateSpaceGeneration.ProgramState;
+import de.rwth.i2.attestor.stateSpaceGeneration.StateSpace;
 import de.rwth.i2.attestor.util.Pair;
 
 public class AbstractMethodIPA extends AbstractMethod {
 
+	private Map<Pair<String, HeapConfiguration>, Integer> knownMethodCalls;
+	private Map<Integer,List<HeapConfiguration>> returnStates;
+	private Map<Integer,Pair<Integer,Integer>> callers;
+	private Map<Integer,StateSpace> stateSpaces;
+	
 	public AbstractMethodIPA(String displayName, StateSpaceFactory factory) {
 		super(displayName, factory);
 	}
@@ -16,6 +22,17 @@ public class AbstractMethodIPA extends AbstractMethod {
 	@Override
 	public Set<ProgramState> getResult(HeapConfiguration input, int scopeDepth) {
 		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<ProgramState> getResult( int idOfCurrentState, int idOfCurrentStateSpace, 
+			String methodSignature ) {
+		
+		StateSpace currentStateSpace = stateSpaces.get( idOfCurrentStateSpace );
+		ProgramState currentState = currentStateSpace.getState( idOfCurrentState );
+		
+		Pair<HeapConfiguration,HeapConfiguration> methodConfig = prepareInput( currentState.getHeap() );
+		//TODO store replaced fragment somewhere
 		return null;
 	}
 
