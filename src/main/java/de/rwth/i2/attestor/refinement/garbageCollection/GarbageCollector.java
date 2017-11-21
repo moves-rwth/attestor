173f8fafe6858368f166ed699171b7b31e91d864
Christoph <matheja@cs.rwth-aachen.de>
2017-11-21 09:49:02 +0100

    Implementation of StateRefinementStrategy for GarbageCollection.

--- src/main/java/de/rwth/i2/attestor/refinement/garbageCollection/GarbageCollector.java
+++ src/main/java/de/rwth/i2/attestor/refinement/garbageCollection/GarbageCollector.java
@@ -3,6 +3,9 @@ package de.rwth.i2.attestor.refinement.garbageCollection;
 import de.rwth.i2.attestor.graph.heap.HeapConfiguration;
 import de.rwth.i2.attestor.graph.heap.HeapConfigurationBuilder;
 import de.rwth.i2.attestor.graph.util.ReachabilityChecker;
+import de.rwth.i2.attestor.semantics.jimpleSemantics.jimple.statements.AssignInvoke;
+import de.rwth.i2.attestor.semantics.jimpleSemantics.jimple.statements.AssignStmt;
+import de.rwth.i2.attestor.semantics.jimpleSemantics.jimple.statements.InvokeStmt;
 import de.rwth.i2.attestor.stateSpaceGeneration.ProgramState;
 import de.rwth.i2.attestor.stateSpaceGeneration.Semantics;
 import de.rwth.i2.attestor.stateSpaceGeneration.StateRefinementStrategy;
@@ -12,12 +15,30 @@ import gnu.trove.set.hash.TIntHashSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GarbageCollector implements StateRefinementStrategy {
 
     private final static Logger logger = LogManager.getLogger("GarbageCollector");
 
+    private final static Set<Class<?>> semanticsTriggeringGarbageCollector = new HashSet<>();
+
+    static {
+        semanticsTriggeringGarbageCollector.add(InvokeStmt.class);
+        semanticsTriggeringGarbageCollector.add(AssignStmt.class);
+        semanticsTriggeringGarbageCollector.add(AssignInvoke.class);
+    }
+
     @Override
     public ProgramState refine(Semantics semantics, ProgramState state) {
+
+        // If the previously executed program statement cannot alter the heap
+        // there is no reason to invoke the garbage collection
+
+        if(!semanticsTriggeringGarbageCollector.contains(semantics.getClass())) {
+           return state;
+        }
 
         HeapConfiguration heap = state.getHeap();
         ReachabilityChecker checker = new ReachabilityChecker(
@@ -40,7 +61,7 @@ public class GarbageCollector implements StateRefinementStrategy {
 
     }
 
-    TIntSet getVariableTargetNodes(HeapConfiguration heap) {
+    private TIntSet getVariableTargetNodes(HeapConfiguration heap) {
 
         TIntSet variableTargets = new TIntHashSet(heap.countVariableEdges());
         TIntIterator variableIterator = heap.variableEdges().iterator();
