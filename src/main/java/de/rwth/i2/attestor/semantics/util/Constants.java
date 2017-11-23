8f1056f36e5062629822e0f08896c1ddc65b812e
Hannah Arndt <hannah.arndt@medemsand.de>
2017-11-23 18:40:24 +0100

    isConstantType in Constants

--- src/main/java/de/rwth/i2/attestor/semantics/util/Constants.java
+++ src/main/java/de/rwth/i2/attestor/semantics/util/Constants.java
@@ -8,6 +8,13 @@ public final class Constants {
     public static final String MINUS_ONE = "-1";
     public static final String TRUE = "true";
     public static final String FALSE = "false";
+    
+    public static final String INT = "int";
+    public static final String INT_0 = "int_0";
+    public static final String INT_1 = "int_1";
+    public static final String INT_M1 = "int_-1";
+    public static final String BOOLEAN = "boolean";
+    public static final String NULL_TYPE = "NULL";
 
     public static boolean isConstant(String name) {
 
@@ -22,5 +29,21 @@ public final class Constants {
             default:
                 return false;
         }
+    }
+    
+    public static boolean isConstantType( String name ) {
+    	
+    	switch( name ) {
+    		case INT:
+    		case INT_0:
+    		case INT_1:	
+    		case INT_M1:
+    		case BOOLEAN:
+    		case NULL_TYPE:
+    			return true;
+    		default:
+    			return false;
+    	}
+    	
     }
 }
