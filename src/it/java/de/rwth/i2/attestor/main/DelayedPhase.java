ef6202557f130bbdf236f5c74089cdcc0227819e
Christoph <matheja@cs.rwth-aachen.de>
2018-01-15 08:57:44 +0100

    Added alternative Main class to simplify profiling.

---
+++ src/it/java/de/rwth/i2/attestor/main/DelayedPhase.java
@@ -0,0 +1,44 @@
+package de.rwth.i2.attestor.main;
+
+import java.io.IOException;
+
+public class DelayedPhase extends AbstractPhase {
+
+    private AbstractPhase phase;
+    private long delay;
+
+    public DelayedPhase(AbstractPhase phase, long delay) {
+
+        super(phase.scene());
+        this.phase = phase;
+        this.delay = delay;
+    }
+
+    @Override
+    public String getName() {
+        return phase.getName();
+    }
+
+    @Override
+    protected void executePhase() throws IOException {
+
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        phase.executePhase();
+    }
+
+    @Override
+    public void logSummary() {
+
+        phase.logSummary();
+    }
+
+    @Override
+    public boolean isVerificationPhase() {
+        return phase.isVerificationPhase();
+    }
+}
---
+++ src/it/java/de/rwth/i2/attestor/main/MainForVisualVM.java
@@ -0,0 +1,39 @@
+package de.rwth.i2.attestor.main;
+
+import de.rwth.i2.attestor.phases.counterexamples.CounterexampleGenerationPhase;
+import de.rwth.i2.attestor.phases.modelChecking.ModelCheckingPhase;
+import de.rwth.i2.attestor.phases.parser.*;
+import de.rwth.i2.attestor.phases.preprocessing.AbstractionPreprocessingPhase;
+import de.rwth.i2.attestor.phases.preprocessing.GrammarRefinementPhase;
+import de.rwth.i2.attestor.phases.preprocessing.MarkingGenerationPhase;
+import de.rwth.i2.attestor.phases.report.ReportGenerationPhase;
+import de.rwth.i2.attestor.phases.report.ReportOutputPhase;
+import de.rwth.i2.attestor.phases.symbolicExecution.nonRecursive.StateSpaceGenerationPhase;
+
+public class MainForVisualVM extends AbstractAttestor{
+
+    public static void main(String[] args) {
+
+        AbstractAttestor main = new MainForVisualVM();
+        main.run(args);
+    }
+
+    @Override
+    protected void registerPhases(String[] args) {
+
+        registry.addPhase(new CLIPhase(scene, args));
+        registry.addPhase(new ParseProgramPhase(scene));
+        registry.addPhase(new ParseGrammarPhase(scene));
+        registry.addPhase(new ParseInputPhase(scene));
+        registry.addPhase(new ParseContractsPhase(scene));
+        registry.addPhase(new MarkingGenerationPhase(scene));
+        registry.addPhase(new GrammarRefinementPhase(scene));
+        registry.addPhase(new AbstractionPreprocessingPhase(scene));
+        registry.addPhase(new DelayedPhase(new StateSpaceGenerationPhase(scene), 5000));
+        registry.addPhase(new ModelCheckingPhase(scene));
+        registry.addPhase(new CounterexampleGenerationPhase(scene));
+        registry.addPhase(new ReportGenerationPhase(scene));
+        registry.addPhase(new ReportOutputPhase(scene, registry.getPhases()));
+        registry.execute();
+    }
+}
