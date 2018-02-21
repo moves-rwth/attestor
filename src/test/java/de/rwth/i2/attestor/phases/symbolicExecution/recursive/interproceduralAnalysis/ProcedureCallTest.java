5382c495d9b8024da2592e20f3f7f25d3676ae1a
hannah.arndt <hannah.arndt@rwth-aachen.de>
2018-02-21 12:27:03 +0100

    Test that ProcedureCall.execute() adds  the expected contract.

--- src/test/java/de/rwth/i2/attestor/phases/symbolicExecution/recursive/interproceduralAnalysis/ProcedureCallTest.java
+++ src/test/java/de/rwth/i2/attestor/phases/symbolicExecution/recursive/interproceduralAnalysis/ProcedureCallTest.java
@@ -1,12 +1,26 @@
 package de.rwth.i2.attestor.phases.symbolicExecution.recursive.interproceduralAnalysis;
 
-import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import de.rwth.i2.attestor.MockupSceneObject;
 import de.rwth.i2.attestor.graph.heap.HeapConfiguration;
-import de.rwth.i2.attestor.main.scene.Scene;
+import de.rwth.i2.attestor.graph.heap.internal.ExampleHcImplFactory;
+import de.rwth.i2.attestor.main.scene.SceneObject;
 import de.rwth.i2.attestor.phases.symbolicExecution.procedureImpl.StateSpaceGeneratorFactory;
 import de.rwth.i2.attestor.phases.symbolicExecution.recursive.InternalProcedureCall;
 import de.rwth.i2.attestor.phases.symbolicExecution.stateSpaceGenerationImpl.InternalStateSpace;
+import de.rwth.i2.attestor.procedures.Contract;
 import de.rwth.i2.attestor.procedures.Method;
 import de.rwth.i2.attestor.stateSpaceGeneration.Program;
 import de.rwth.i2.attestor.stateSpaceGeneration.ProgramState;
@@ -14,23 +28,40 @@ import de.rwth.i2.attestor.stateSpaceGeneration.StateSpace;
 import de.rwth.i2.attestor.stateSpaceGeneration.StateSpaceGenerationAbortedException;
 import de.rwth.i2.attestor.stateSpaceGeneration.StateSpaceGenerator;
 
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.*;
-
 public class ProcedureCallTest {
 
+	SceneObject sceneObject = new MockupSceneObject();
+	
 	@Test
 	public void testExecute() {
 		ProcedureRegistry registry = mock(ProcedureRegistry.class);
 		
+		
+		Method methodMock = mock(Method.class);
+	
+		HeapConfiguration preconditionDummy = new HeapConfigurationDummy("precondition");
+		
+		HeapConfigurationDummy finalHeap1 = new HeapConfigurationDummy("f1");
+		ProgramState finalStateDummy1 = sceneObject.scene().createProgramState( finalHeap1 );
+		HeapConfigurationDummy finalHeap2 = new HeapConfigurationDummy("f2");
+		ProgramState finalStateDummy2 = sceneObject.scene().createProgramState( finalHeap2 );
 		StateSpace fakeResult = new InternalStateSpace(3);
+		fakeResult.addState( finalStateDummy1  );
+		fakeResult.setFinal( finalStateDummy1 );
+		fakeResult.addState( finalStateDummy2 );
+		fakeResult.setFinal( finalStateDummy2 );
 		StateSpaceGeneratorFactory fakeFaktory = createFakeFactory(fakeResult);
 		
-		ProcedureCall call = new InternalProcedureCall( mock(Method.class), null, fakeFaktory, registry );
+		ProcedureCall call = new InternalProcedureCall( methodMock, preconditionDummy, fakeFaktory, registry );
 		call.execute();
 		
 		verify(registry).registerStateSpace( call, fakeResult );
 		verifyNoMoreInteractions( registry );
+		
+		final ArgumentCaptor<Contract> captor = ArgumentCaptor.forClass(Contract.class);
+		verify( methodMock ).addContract( captor.capture() );
+		assertEquals( preconditionDummy, captor.getValue().getPrecondition() );
+		assertThat( captor.getValue().getPostconditions(), containsInAnyOrder( finalHeap1, finalHeap2) );
 	}
 
 	/**
@@ -49,12 +80,10 @@ public class ProcedureCallTest {
 			fail("As the method is mocked, it should not invoke an exception");
 		}
 		
-		Scene fakeScene = mock(Scene.class );
-		when( fakeScene.createProgramState( any() ) ).thenReturn( mock( ProgramState.class ));
 		
 		StateSpaceGeneratorFactory fakeFaktory = mock(StateSpaceGeneratorFactory.class);
 		when( fakeFaktory.create(any(Program.class), any(ProgramState.class)) ).thenReturn( fakeGenerator );
-		when( fakeFaktory.scene() ).thenReturn( fakeScene );
+		when( fakeFaktory.scene() ).thenReturn( sceneObject.scene() );
 		return fakeFaktory;
 	}
 
