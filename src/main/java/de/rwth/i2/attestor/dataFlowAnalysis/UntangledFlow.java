02bd2f74f7386c044d75d71de5eb93806d566baa
Mohamed Khalifa <mohamed.khalifa@rwth-aachen.de>
2020-05-05 13:37:54 +0200

    PA-6 updated UntangledFlow add method

--- src/main/java/de/rwth/i2/attestor/dataFlowAnalysis/UntangledFlow.java
+++ src/main/java/de/rwth/i2/attestor/dataFlowAnalysis/UntangledFlow.java
@@ -3,16 +3,29 @@ package de.rwth.i2.attestor.dataFlowAnalysis;
 import gnu.trove.list.array.TIntArrayList;
 
 public class UntangledFlow extends FlowImpl {
+    final int copy;
+    final int untangled;
+
     public UntangledFlow(FlowImpl flow, int untangle) {
         super(flow);
-
-        int untangled = new TIntArrayList(getLabels().toArray()).max() + 1;
+        this.untangled = untangle;
+        this.copy = new TIntArrayList(getLabels().toArray()).max() + 1;
 
         getPredecessors(untangle).forEach(l -> {
             remove(l, untangle);
-            add(l, untangled);
+            add(l, copy);
 
             return true;
         });
+    }
+
+    public void add(int from, int to) {
+        if (from == copy) {
+            super.add(untangled, to);
+        } else if (to == untangled) {
+            super.add(from, copy);
+        } else {
+            super.add(from, to);
+        }
     }
 }
