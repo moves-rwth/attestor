fb43a851a13517128dfb5ee33d8b6609995aa547
Mohamed Khalifa <mohamed.khalifa@rwth-aachen.de>
2020-05-05 10:09:14 +0200

    PA-17 implemented transfer functions for PredicateAnalysis

--- src/main/java/de/rwth/i2/attestor/dataFlowAnalysis/predicate/PredicateAnalysis.java
+++ src/main/java/de/rwth/i2/attestor/dataFlowAnalysis/predicate/PredicateAnalysis.java
@@ -4,14 +4,18 @@ import de.rwth.i2.attestor.dataFlowAnalysis.DataFlowAnalysis;
 import de.rwth.i2.attestor.dataFlowAnalysis.Flow;
 import de.rwth.i2.attestor.dataFlowAnalysis.UntangledFlow;
 import de.rwth.i2.attestor.domain.AssignMapping;
+import de.rwth.i2.attestor.domain.AssignMappingImpl;
 import de.rwth.i2.attestor.domain.Lattice;
 import de.rwth.i2.attestor.domain.RelativeIndexSet;
+import de.rwth.i2.attestor.graph.heap.internal.TAHeapConfiguration;
+import de.rwth.i2.attestor.graph.heap.internal.TransformationStep;
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 
 import java.util.Collections;
+import java.util.Queue;
 import java.util.function.Function;
 
 public class PredicateAnalysis<I, L extends Lattice<I> & RelativeIndexSet<I>>
@@ -22,11 +26,18 @@ public class PredicateAnalysis<I, L extends Lattice<I> & RelativeIndexSet<I>>
     private final int extremalLabel;
     private final L indexLattice;
     private final Lattice<AssignMapping<Integer, I>> domainLattice;
+    private final IndexAbstractionRule<I> indexAbstractionRule;
 
-    public PredicateAnalysis(L indexLattice, StateSpaceAdapter adapter, int extremalLabel) {
+    public PredicateAnalysis(
+            int extremalLabel,
+            L indexLattice,
+            StateSpaceAdapter adapter,
+            IndexAbstractionRule<I> indexAbstractionRule) {
+
+        this.extremalLabel = extremalLabel;
+        this.indexAbstractionRule = indexAbstractionRule;
         this.flow = new UntangledFlow(adapter.flow, extremalLabel);
         this.labelToState = adapter.labelToStateMap;
-        this.extremalLabel = extremalLabel;
         this.indexLattice = indexLattice;
         this.domainLattice = new AssignMapping.AssignMappingSet<>(indexLattice);
     }
@@ -64,6 +75,69 @@ public class PredicateAnalysis<I, L extends Lattice<I> & RelativeIndexSet<I>>
 
     @Override
     public Function<AssignMapping<Integer, I>, AssignMapping<Integer, I>> getTransferFunction(int from, int to) {
-        return null;
+        TAProgramState state = labelToState.get(to);
+
+        if (state.isMaterialized) {
+            return generateMaterializationTransferFunction(state.heap);
+        } else {
+            return generateAbstractionTransferFunction(state.heap);
+        }
+    }
+
+    private Function<AssignMapping<Integer, I>, AssignMapping<Integer, I>>
+    generateMaterializationTransferFunction(TAHeapConfiguration heap) {
+        final Queue<TransformationStep> history = heap.transformationHistory;
+
+        return assign -> {
+            AssignMappingImpl<Integer, I> result = new AssignMappingImpl<>(assign);
+            while (!history.isEmpty()) {
+                TransformationStep step = history.element();
+
+                TIntObjectMap<I> fragment = indexAbstractionRule.abstractForward(
+                        result.apply(step.getNtEdge()),
+                        step.getRule());
+
+                // map result from rule to actual heap
+                TIntObjectHashMap<I> matchedFragment = new TIntObjectHashMap<>();
+                fragment.forEachEntry((key, value) -> {
+                    matchedFragment.put(step.match(key), value);
+                    return true;
+                });
+
+                // update assign mapping
+                matchedFragment.forEachEntry((key, value) -> {
+                    result.set(key, value);
+                    return true;
+                });
+            }
+
+            return result;
+        };
+    }
+
+    private Function<AssignMapping<Integer, I>, AssignMapping<Integer, I>>
+    generateAbstractionTransferFunction(TAHeapConfiguration heap) {
+        final Queue<TransformationStep> history = heap.transformationHistory;
+
+        return assign -> {
+            AssignMappingImpl<Integer, I> result = new AssignMappingImpl<>(assign);
+
+            while (!history.isEmpty()) {
+                TransformationStep step = history.element();
+
+                // map current value from actual heap to rule
+                TIntObjectHashMap<I> fragment = new TIntObjectHashMap<I>();
+                for (Integer key : result) {
+                    fragment.put(step.match(key), result.apply(key));
+                }
+
+                I newIndex = indexAbstractionRule.abstractBackward(fragment, step.getRule());
+
+                // update assign mapping
+                result.set(step.getNtEdge(), newIndex);
+            }
+
+            return result;
+        };
     }
 }
--- src/main/java/de/rwth/i2/attestor/domain/AssignMappingImpl.java
+++ src/main/java/de/rwth/i2/attestor/domain/AssignMappingImpl.java
@@ -1,16 +1,31 @@
 package de.rwth.i2.attestor.domain;
 
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class AssignMappingImpl<S, I> extends AssignMapping<S, I> {
-    private final Map<S, I> backend;
+    private final Map<S, I> backend = new HashMap<>();
 
-    public AssignMappingImpl(Map<S, I> backend) {
-        this.backend = backend;
+    public AssignMappingImpl(AssignMapping<S, I> assignMapping) {
+        for (S key : assignMapping) {
+            backend.put(key, assignMapping.apply(key));
+        }
+    }
+
+    public I set(S key, I value) {
+        return backend.put(key, value);
     }
 
     @Override
     public I apply(S s) {
         return backend.get(s);
+    }
+
+    @Nonnull
+    @Override
+    public Iterator<S> iterator() {
+        return backend.keySet().iterator();
     }
 }
