66893280529d707c3c27dac19572b5440d097c96
Mohamed Khalifa <mohamed.khalifa@rwth-aachen.de>
2020-07-27 13:27:47 +0200

    PA-31 generalized widening

--- src/main/java/de/rwth/i2/attestor/domain/RelativeIndexSupplier.java
+++ src/main/java/de/rwth/i2/attestor/domain/RelativeIndexSupplier.java
@@ -1,8 +1,9 @@
 package de.rwth.i2.attestor.domain;
 
 import java.util.Set;
+import java.util.function.Supplier;
 
-public interface RelativeIndexSupplier<T, I extends RelativeIndex<T>> {
+public interface RelativeIndexSupplier<T, I extends RelativeIndex<T>> extends Supplier<I> {
     I get();
 
     I get(T value);
